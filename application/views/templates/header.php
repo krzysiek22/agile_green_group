4de266eb0b2669f772d4b342488e8afbccde8739
Jacek Kazimierski <jacek.kazimierski@abastra.com>
2017-10-29 18:54:07 +0100

    UserDAO: CRUD Operations

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,10 +13,12 @@
     <meta name="author" content="">
     <!-- <link rel="icon" href="../../favicon.ico"> -->
 
+    <link rel="icon" href="<?= base_url('assets/favicon.png'); ?>">
     <title>Agile Green Group</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?= base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="<?= base_url('assets/css/main.css'); ?>" rel="stylesheet">
@@ -28,19 +30,19 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Agile Green Group</a>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
+          <!--
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#"><i class="fa fa-home fa-2x"></i></a>
           </li>
+          -->
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link disabled" href="#">Job Offers</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
+          <!--
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
             <div class="dropdown-menu" aria-labelledby="dropdown01">
@@ -49,10 +51,13 @@
               <a class="dropdown-item" href="#">Something else here</a>
             </div>
           </li>
+          -->
         </ul>
+        <!--
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        -->
       </div>
     </nav>
